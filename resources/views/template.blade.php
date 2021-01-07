8f287702ecad7a6ce366fccabf6094171180e115
FikryAS <azizfikry15@gmail.com>
2021-01-07 11:09:12 +0700

    membuat template untuk dipakai di halaman lainnya tanpa mengulan code

---
+++ resources/views/template.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  <title>Data Siswa</title>
+</head>
+
+<body>
+  <nav class="navbar navbar-expand bg-dark">
+    <ul class="nav">
+      <li>
+        <a href="{{ url('/beranda') }}" class="nav-link">Home</a>
+      </li>
+      <li>
+        <a href="{{ url('/data-siswa') }}" class="nav-link">Data Siswa</a>
+      </li>
+      <li>
+        <a href="{{ url('/info-kegiatan') }}" class="nav-link">Info Kegiatan</a>
+      </li>
+    </ul>
+  </nav>
+  @yield('content')
+
+  <footer class="fixed-bottom bg-primary">
+    <div class="text-center">
+      (c) 2021 Copyright : SMA Contoh
+    </div>
+  </footer>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+</body>
+
+</html>
