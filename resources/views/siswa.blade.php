cca01996a23cdfbdf9f49ab2a25eee0d9b21c3c2
FikryAS <azizfikry15@gmail.com>
2021-01-07 11:09:59 +0700

    implementasi template di dalam tampilan home, info siswa, dan data siswa

---
+++ resources/views/siswa.blade.php
@@ -0,0 +1,24 @@
+@extends('template')
+
+@section('content')
+<h3>Data Siswa</h3>
+<table class="table-bordered table-striped">
+  <tr>
+    <th>Name</th>
+    <th>NIS</th>
+    <th>Tanggal Lahir</th>
+  </tr>
+
+  <tr>
+    <td>Fikry AS</td>
+    <td>101</td>
+    <td>1999-09-07</td>
+  </tr>
+
+  <tr>
+    <td>Maulana</td>
+    <td>102</td>
+    <td>1999-12-07</td>
+  </tr>
+</table>
+@endsection
