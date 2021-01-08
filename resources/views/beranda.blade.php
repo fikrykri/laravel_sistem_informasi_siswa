ecb81a421b039c49ab4b5f9873eee49128c49839
FikryAS <azizfikry15@gmail.com>
2021-01-08 17:53:32 +0700

    menambahkan izin untuk admin pada manu data siswa dan manajemen user

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -4,7 +4,10 @@
 <div class="jumbotron text-center">
   <h1>Portal Informasi Siswa</h1>
   <p>Selamat datang di Portal Informasi Siswa SMA Contoh!</p>
-  <a href="#" class="btn btn-dark">Info Kegiatan</a>
-  <a href="#" class="btn btn-primary">Data Siswa</a>
+  <a href="{{ url('/info-kegiatan') }}" class="btn btn-dark">Info Kegiatan</a>
+  @can('isAdmin')
+  <a href="{{ route('siswa.index') }}" class="btn btn-primary">Data Siswa</a>
+  <a href="{{ route('users.index') }}" class="btn btn-warning">Data User</a>
+  @endcan
 </div>
 @endsection
