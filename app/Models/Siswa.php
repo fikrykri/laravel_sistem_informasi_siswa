254ad8ec24ea78d8f8da42abbf5495fbd6e292d2
FikryAS <azizfikry15@gmail.com>
2021-01-08 13:31:35 +0700

    menambah izin untuk menambah data pada setiap field

--- app/Models/Siswa.php
+++ app/Models/Siswa.php
@@ -10,4 +10,7 @@ class Siswa extends Model
     use HasFactory;
 
     protected $table = 'siswa';
+
+    // memberi izin untuk mengisi setiap kolom nama,nis,dan tgl_lahir
+    protected $fillable = ['nama', 'nis', 'tgl_lahir'];
 }
